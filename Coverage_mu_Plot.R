4aaa53880ee3bc08d72d23e925a3406b166a9548
Alessandra <asaracini@ethz.ch>
2023-08-03 21:48:48 +0200

    plot legend

--- Coverage_mu_Plot.R
+++ Coverage_mu_Plot.R
@@ -8,7 +8,7 @@ wrap_plot_cov <- function(data) {
     geom_line(aes(x = mu_values, y = cov.adj.wrong, color = "Copas HR+LR"), linewidth = 0.6) +
     geom_line(aes(x = mu_values, y = cov.unadj, color = "Unadjusted"), linewidth = 0.6) +
     geom_line(aes(x = mu_values, y = cov.adj.sigmoid, color = "Sigmoid HR+LR"), linewidth = 0.6) +
-    #geom_hline(yintercept = 0, color = "black", linetype = "dashed", linewidth = 0.4) +
+    geom_hline(yintercept = 0, color = "black", linetype = "dashed", linewidth = 0.4) +
     labs(
       x = expression(mu),
       y = expression(E(hat(mu)) - mu),
@@ -23,6 +23,23 @@ wrap_plot_cov <- function(data) {
       plot.title = element_text(size = 7)
     )
 }
+
+# Create a separate plot for the legend
+legend_plot_bias <- ggplot() +
+  geom_line(aes(x = 0, y = 0, color =  "Unadjusted")) +
+  geom_line(aes(x = 0, y = 0, color =  "Copas HR")) +
+  geom_line(aes(x = 0, y = 0, color =  "Neg Exp HR+LR")) +
+  geom_line(aes(x = 0, y = 0, color = "Copas HR+LR"))+
+  geom_line(aes(x = 0, y = 0, color = "Sigmoid HR+LR"))+
+
+  theme_void()+
+  theme(
+    legend.text = element_text(size = 4),
+    legend.key.size = unit(2, "mm"), # Adjust the size of plot title
+    axis.title.x = element_blank(),             # Remove x-axis label
+    axis.title.y = element_blank()
+  )+
+  guides(color = guide_legend(title = NULL))
 
 
 # Generate plots
